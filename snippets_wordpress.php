c29fb1ea52ff73fc899178c2450413deaa4530a6
Simone <f.simone89@gmail.com>
2022-04-07 15:23:20 +0200

    Add custom post_type

--- snippets_wordpress.php
+++ snippets_wordpress.php
@@ -1,4 +1,3 @@
-
 <!--
 ========================
 How to use wp_query
@@ -19,9 +18,9 @@ $lastBlog = new WP_Query($args); //inizializzo una variabile passando la creazio
 if($lastBlog->have_posts())://se la $var ha dei posto
     while($lastBlog->have_posts()): $lastBlog->the_post(); // carica i post?>
 
-    <!--Stapa qui il tuo contenuto-->
-    
-    <?php endwhile;
+<!--Stapa qui il tuo contenuto-->
+
+<?php endwhile;
     endif;
     wp_reset_postdata( );
     ?>
@@ -43,10 +42,39 @@ function nome_custom_post_type(){
 
     $labels = array(
         'name' => 'nome del cpt',//non del custom post type viene visualizzato tip pagine/articoli
-        'singular_name' => 'nome al singolare'//tenere uguale al plurale è più comodo
-        ''
+        'singular_name' => 'nome al singolare,'//tenere uguale al plurale è più comodo
+        'add_new'=>'Add Portfolio Item',//label for the button
+        'all_items' => 'All Items',
+		'add_new_item' => 'Add Item',
+		'edit_item' => 'Edit Item',
+		'new_item' => 'New Item',
+		'view_item' => 'View Item',
+		'search_item' => 'Search Portfolio',
+		'not_found' => 'No items found',
+		'not_found_in_trash' => 'No items found in trash',
+		'parent_item_colon' => 'Parent Item'
     );
-    $args
+    $args = array(
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail',
+			'revisions',
+		),
+		'taxonomies' => array('category', 'post_tag'),
+		'menu_position' => 5,
+		'exclude_from_search' => false
+	);
+	register_post_type('portfolio',$args);
 
 
 
@@ -54,7 +82,7 @@ function nome_custom_post_type(){
 
 
 }
+add_action('init','nome_custom_post_type' )
 
 
 ?>
-
